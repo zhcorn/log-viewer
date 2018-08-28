aa034d74f296103027c0927d66de8529c94a2875
zhcorn <zhcorn@gmail.com>
2018-08-28 16:56:53 +0800

    change views to AdminLTE 3.0.0

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -75,8 +75,8 @@
 
     <!-- /.col -->
     <div class="col-md-10">
-        <div class="box box-primary">
-            <div class="box-header with-border">
+        <div class="card card-primary card-outline">
+            <div class="card-header with-border">
                 <button type="button" class="btn btn-primary btn-sm log-refresh"><i class="fa fa-refresh"></i> {{ trans('admin.refresh') }}</button>
                 <button type="button" class="btn btn-default btn-sm log-live"><i class="fa fa-play"></i> </button>
                 <div class="pull-right">
@@ -90,10 +90,10 @@
                     </div>
                     <!-- /.btn-group -->
                 </div>
-                <!-- /.box-tools -->
+                <!-- /.card-tools -->
             </div>
-            <!-- /.box-header -->
-            <div class="box-body no-padding">
+            <!-- /.card-header -->
+            <div class="card-body no-padding">
 
                 <div class="table-responsive">
                     <table class="table table-hover">
@@ -113,13 +113,13 @@
                         @foreach($logs as $index => $log)
 
                             <tr>
-                                <td><span class="label bg-{{\Encore\Admin\LogViewer\LogViewer::$levelColors[$log['level']]}}">{{ $log['level'] }}</span></td>
+                                <td><span class="badge bg-{{\Encore\Admin\LogViewer\LogViewer::$levelColors[$log['level']]}}">{{ $log['level'] }}</span></td>
                                 <td><strong>{{ $log['env'] }}</strong></td>
                                 <td style="width:150px;">{{ $log['time'] }}</td>
                                 <td><code style="word-break: break-all;">{{ $log['info'] }}</code></td>
                                 <td>
                                     @if(!empty($log['trace']))
-                                    <a class="btn btn-primary btn-xs" data-toggle="collapse" data-target=".trace-{{$index}}"><i class="fa fa-info"></i>&nbsp;&nbsp;Exception</a>
+                                    <a href="#" class="btn btn-primary btn-xs" data-toggle="collapse" data-target=".trace-{{$index}}"><i class="fa fa-info"></i>&nbsp;&nbsp;Exception</a>
                                     @endif
                                 </td>
                             </tr>
@@ -136,20 +136,20 @@
                     </table>
                     <!-- /.table -->
                 </div>
-                <!-- /.mail-box-messages -->
+                <!-- /.mail-card-messages -->
             </div>
-            <!-- /.box-body -->
+            <!-- /.card-body -->
         </div>
-        <!-- /. box -->
+        <!-- /. card -->
     </div>
 
     <div class="col-md-2">
 
-        <div class="box box-solid">
-            <div class="box-header with-border">
-                <h3 class="box-title">Files</h3>
+        <div class="card card-solid">
+            <div class="card-header with-border">
+                <h3 class="card-title">Files</h3>
             </div>
-            <div class="box-body no-padding">
+            <div class="card-body no-padding">
                 <ul class="nav nav-pills nav-stacked">
                     @foreach($logFiles as $logFile)
                         <li @if($logFile == $fileName)class="active"@endif>
@@ -158,14 +158,14 @@
                     @endforeach
                 </ul>
             </div>
-            <!-- /.box-body -->
+            <!-- /.card-body -->
         </div>
 
-        <div class="box box-solid">
-            <div class="box-header with-border">
-                <h3 class="box-title">Info</h3>
+        <div class="card card-solid">
+            <div class="card-header with-border">
+                <h3 class="card-title">Info</h3>
             </div>
-            <div class="box-body no-padding">
+            <div class="card-body no-padding">
                 <ul class="nav nav-pills nav-stacked">
                     <li class="margin: 10px;">
                         <a>Size: {{ $size }}</a>
@@ -175,10 +175,10 @@
                     </li>
                 </ul>
             </div>
-            <!-- /.box-body -->
+            <!-- /.card-body -->
         </div>
 
-        <!-- /.box -->
+        <!-- /.card -->
     </div>
     <!-- /.col -->
 </div>
--- src/LogViewer.php
+++ src/LogViewer.php
@@ -36,14 +36,14 @@ class LogViewer extends Extension
      * @var array
      */
     public static $levelColors = [
-        'EMERGENCY' => 'black',
+        'EMERGENCY' => 'default',
         'ALERT'     => 'navy',
         'CRITICAL'  => 'maroon',
-        'ERROR'     => 'red',
-        'WARNING'   => 'orange',
-        'NOTICE'    => 'light-blue',
-        'INFO'      => 'aqua',
-        'DEBUG'     => 'green',
+        'ERROR'     => 'danger',
+        'WARNING'   => 'warning',
+        'NOTICE'    => 'primary',
+        'INFO'      => 'info',
+        'DEBUG'     => 'success',
     ];
 
     /**
